d65d0d8ffc85487c66b6867cb2a39b3d81d8f8e3
Mark Grevtsov <markgrevtsov2310@gmail.com>
2024-04-18 23:06:25 +0300

    fixed prometheus config

--- internal/config/prometheus.go
+++ internal/config/prometheus.go
@@ -1,6 +1,6 @@
 package config
 
 type Prometheus struct {
-	Host string `env:"REST_SERVER_PROMETHEUS_HOST" env-required:"true"`
-	Port int    `env:"REST_SERVER_PROMETHEUS_PORT" env-required:"true"`
+	Host string `env:"REST_SERVER_PROMETHEUS_HOST" env-default:"0.0.0.0"`
+	Port int    `env:"REST_SERVER_PROMETHEUS_PORT" env-default:"8082"`
 }
